f91279afbf4e09ac5ced060d7a13267d6e70a930
Shi Ning Le <shining@ualberta.ca>
2022-11-07 00:21:54 -0700

    Update interfaces.ts

--- features/annotations/interfaces.ts
+++ features/annotations/interfaces.ts
@@ -1,7 +1,4 @@
-interface Vehicle {
-    name: string;
-    year: Date;
-    broken: boolean;
+interface Reportable {
     summary(): string;
 }
 
@@ -19,8 +16,8 @@ const oldCivic = {
 //     year: number;
 //     broken: boolean;
 // }
-const printVehicle = (vehicle: Vehicle): void => {
-    console.log(vehicle.summary());
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary());
 };
 
-printVehicle(oldCivic);
+printSummary(oldCivic);
