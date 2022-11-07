eaa094438a297d174573058c7c2faa3618b5de32
Shi Ning Le <shining@ualberta.ca>
2022-11-07 13:33:40 -0700

    Create Company.ts

---
+++ maps/src/Company.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker';
+
+export class Company {
+    name: string;
+    catchPhrase: string;
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+    constructor() {
+        this.name = faker.company.name();
+        this.catchPhrase = faker.company.catchPhrase();
+        this.location = {
+            latitude: parseFloat(faker.address.latitude()),
+            longitude: parseFloat(faker.address.longitude()),
+        };
+    }
+}
