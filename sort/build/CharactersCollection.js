2d1083ef0df913565d5e8e79f4bc86c6335c218f
Shi Ning Le <shining@ualberta.ca>
2022-11-09 23:41:53 -0700

    implement characters collection

--- sort/build/CharactersCollection.js
+++ sort/build/CharactersCollection.js
@@ -8,7 +8,16 @@ class CharactersCollection {
     get length() {
         return this.data.length;
     }
-    compare(leftIndex, rightIndex) { }
-    swap(leftIndex, rightIndex) { }
+    compare(leftIndex, rightIndex) {
+        return (this.data[leftIndex].toLowerCase() >
+            this.data[rightIndex].toLowerCase());
+    }
+    swap(leftIndex, rightIndex) {
+        const characters = this.data.split('');
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = leftHand;
+        this.data = characters.join('');
+    }
 }
 exports.CharactersCollection = CharactersCollection;
--- sort/src/CharactersCollection.ts
+++ sort/src/CharactersCollection.ts
@@ -1,8 +1,22 @@
-export class CharactersCollection {
+import { Sortable } from './Sorter';
+export class CharactersCollection implements Sortable {
     constructor(public data: string) {}
     get length(): number {
         return this.data.length;
     }
-    compare(leftIndex: number, rightIndex: number): boolean {}
-    swap(leftIndex: number, rightIndex: number): void {}
+    compare(leftIndex: number, rightIndex: number): boolean {
+        return (
+            this.data[leftIndex].toLowerCase() >
+            this.data[rightIndex].toLowerCase()
+        );
+    }
+    swap(leftIndex: number, rightIndex: number): void {
+        const characters = this.data.split('');
+
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = leftHand;
+
+        this.data = characters.join('');
+    }
 }
