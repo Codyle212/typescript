51b1b14c9fb5aecbf1d00df97216c4c42522bd5a
Shi Ning Le <shining@ualberta.ca>
2022-11-09 23:27:15 -0700

    add sortable interface

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,12 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 
+interface Sortable {
+    length: number;
+    compare(leftIndex: number, rightIndex: number): boolean;
+    swap(leftIndex: number, rightIndex: number): void;
+}
+
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
