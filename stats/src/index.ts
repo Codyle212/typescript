ecd90ff5ab59e766a60311c64846450e49a463a7
Shi Ning Le <shining@ualberta.ca>
2022-11-15 14:54:33 -0700

    remove the analyzer layer

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,19 +1,13 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from './MatchResult';
+import { Summary } from './Summary';
 
 //Create an object that satisfies the DataReader interface
-const csvFileReader = new CsvFileReader('football.csv');
+/*const csvFileReader = new CsvFileReader('football.csv');*/
 //Create an instance of MatchReader and pass in something satisfies the DataReader interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
-let manUnitedWins = 0;
 
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-        manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(manUnitedWins);
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
+summary.buildAndPrintReport(matchReader.matches);
