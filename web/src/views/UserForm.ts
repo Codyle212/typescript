84cbbe3e7346e1fa750e86d18b3efadc44b71cc3
Shi Ning Le <shining@ualberta.ca>
2022-11-19 13:01:05 -0700

    add rendering and event handing to form

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -1,12 +1,41 @@
 export class UserForm {
-    parent: Element;
+    constructor(public parent: Element) {}
+
+    eventsMaps(): { [key: string]: () => void } {
+        return {
+            'click:button': this.onButtonClick,
+        };
+    }
+
+    onButtonClick(): void {
+        console.log('Button Clicked');
+    }
 
     template(): string {
         return `
             <div>
                 <h1>User Form</h1>
                 <input />
+                <button>Click me</button>
             </div>
         `;
+    }
+
+    bindEvents(fragment: DocumentFragment): void {
+        const eventsMap = this.eventsMaps();
+        for (let eventKey in eventsMap) {
+            const [eventName, selector] = eventKey.split(':');
+            //click:button  ,eventName= click, selector = button
+            fragment.querySelectorAll(selector).forEach((element) => {
+                element.addEventListener(eventName, eventsMap[eventKey]);
+            });
+        }
+    }
+
+    render(): void {
+        const templateElement = document.createElement('template');
+        templateElement.innerHTML = this.template();
+        this.bindEvents(templateElement.content);
+        this.parent.append(templateElement.content);
     }
 }
