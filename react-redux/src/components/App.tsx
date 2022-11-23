107c067c4d614277d485bcee2ad4e51f9e04d769
Shi Ning Le <shining@ualberta.ca>
2022-11-22 22:42:31 -0700

    boiler plate for redux+ env for vite

---
+++ react-redux/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+//use interface to define structure of properties
+interface AppProps {
+    color?: string;
+}
+
+class App extends React.Component {
+    render() {
+        return <div>Hi</div>;
+    }
+}
+export default App;
